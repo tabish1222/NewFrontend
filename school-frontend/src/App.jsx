b678884463221db76c757445c6c394ef4ea092bf
tabish1222 <107319072+tabish1222@users.noreply.github.com>
2025-10-05 15:55:44 +0530

    Update App.jsx

--- school-frontend/src/App.jsx
+++ school-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";  // <-- Add this line
 import StudentsPage from "./pages/StudentsPage";
 import TeachersPage from "./pages/TeachersPage";
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} /> {/* New route */}
         <Route path="/students" element={<StudentsPage />} />
         <Route path="/teachers" element={<TeachersPage />} />
       </Routes>
